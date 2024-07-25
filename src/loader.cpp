09daa34caa70b2f7676787fc1183ad206abcbb84
INKintheHART <inkinthehart@gmail.com>
2024-07-26 06:37:04 +1000

    Update loader.cpp

--- src/loader.cpp
+++ src/loader.cpp
@@ -15,3 +15,13 @@ void AddHardModeScripts();
 //    AddMyPlayerScripts();
 //}
 
+// From SC
+void AddSC_mod_hardmode();
+
+// Add all
+// cf. the naming convention https://github.com/azerothcore/azerothcore-wotlk/blob/master/doc/changelog/master.md#how-to-upgrade-4
+// additionally replace all '-' in the module folder name with '_' here
+void Addmod_hardmodeScripts()
+{
+    AddSC_mod_hardmode();
+}
