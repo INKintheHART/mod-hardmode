8aaf52643f7ff3293da4aaf83c7c6a86212e8b11
INKintheHART <inkinthehart@gmail.com>
2024-07-26 06:46:17 +1000

    Update loader.cpp

--- src/loader.cpp
+++ src/loader.cpp
@@ -3,25 +3,12 @@
  */
 
 // From SC
-//void AddHardModeScripts();
-
-// Add all
-// cf. the naming convention https://github.com/azerothcore/azerothcore-wotlk/blob/master/doc/changelog/master.md#how-to-upgrade-4
-// additionally replace all '-' in the module folder name with '_' here
-
-
-//void Addskeleton_moduleScripts()
-//{
-//    AddMyPlayerScripts();
-//}
-
-// From SC
-void AddSC_mod_hardmode();
+void AddHardModePlayerScripts();
 
 // Add all
 // cf. the naming convention https://github.com/azerothcore/azerothcore-wotlk/blob/master/doc/changelog/master.md#how-to-upgrade-4
 // additionally replace all '-' in the module folder name with '_' here
 void Addmod_hardmodeScripts()
 {
-    AddSC_mod_hardmode();
+    AddHardModePlayerScripts();
 }
